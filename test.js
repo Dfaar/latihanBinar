689d105591157d5c573da6a8d58dbc9511391cda
Dfaar <daffaarifadilah@gmail.com>
2022-03-14 16:21:54 +0700

    testing

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-asd
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const test = () => {
+  return (
+    <View>
+      <Text>test</Text>
+    </View>
+  )
+}
+
+export default test
+
+const styles = StyleSheet.create({})
